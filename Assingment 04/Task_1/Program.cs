a8a7a78d2b3d383c7e7f0cf3b9ff2dc01273709c
cerao <cerao1990@gmail.com>
2023-06-12 15:52:04 +0400

    Add files via upload

---
+++ Assingment 04/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Enter array size");
+int ArraySize, ArrayIndexSun, Index;
+
+
+ArraySize = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter array elements");
+int[] MyArray = new int[ArraySize];
+
+for (int i = 0; i < ArraySize; i++)
+{
+    MyArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Enter index of array");
+Index = int.Parse(Console.ReadLine());
+
+ArrayIndexSun = ArraySum(MyArray, Index);
+
+Console.WriteLine("Sum index of array is "+ ArrayIndexSun);
+
+
+int ArraySum(int[] Array, int Index)
+{
+    var sum = 0;
+
+    if (Array.Length < Index)
+        return -1;
+
+    
+    for (int i = 0; i < Index; i++)
+    {
+        sum += Array[i];
+    }
+
+    return sum;
+}
---
+++ Assingment 04/Task_2/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+
+FillArray();
+
+
+
+
+
+char[] FillArray()
+{
+    Console.WriteLine("Enter size of CHAR array");
+    int size = int.Parse(Console.ReadLine());
+
+
+    var CharArr = new char[size];
+
+    Console.WriteLine("Enter " + size + " symbol");
+    string symbol = Console.ReadLine();
+
+    for (var i = 0; i < size; i++)
+    {
+        CharArr[i] = symbol[i];
+    }
+
+    for (var i = 0; i < size; i++)
+        Console.WriteLine("CharArr[" + i + "] = " + CharArr[i]);
+
+
+    Console.WriteLine("Enter symbol you want to search in array");
+    string SearchSymbolST = Console.ReadLine();
+    char SearchSymbo = SearchSymbolST[0];
+
+    SearchSymbol(CharArr, SearchSymbo);
+
+    return CharArr;
+}
+
+int SearchSymbol(char[] CharArr, char Symbol)
+{
+    int Count = 0;
+    foreach (char c in CharArr)
+    {
+        if (c == Symbol)
+            Count++;
+    }
+
+    CountSymbol(Count, Symbol);
+    return Count;
+}
+
+
+void CountSymbol(int Count, char Symbol)
+{
+    Console.WriteLine("Symbol '" + Symbol + "' was " + Count + " times");
+}
