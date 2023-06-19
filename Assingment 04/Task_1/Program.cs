f09da071ac20bd2df78f8a2f937529bfab0c3f43
cerao <cerao1990@gmail.com>
2023-06-19 16:44:26 +0400

    Update Program.cs

--- Assingment 04/Task_1/Program.cs
+++ Assingment 04/Task_1/Program.cs
@@ -1,37 +1,52 @@
-﻿Console.WriteLine("Enter array size");
-int ArraySize, ArrayIndexSun, Index;
+Console.WriteLine("Enter array size");
+int ArraySize, ArrayIndexSun, Index, ArrayElement;
+bool IsArraySize, IsIndex;
 
+IsArraySize = int.TryParse(Console.ReadLine(), out ArraySize);
 
-ArraySize = int.Parse(Console.ReadLine());
+if (!IsArraySize || ArraySize <= 0) return;
 
 Console.WriteLine("Enter array elements");
 int[] MyArray = new int[ArraySize];
 
+
+
 for (int i = 0; i < ArraySize; i++)
 {
-    MyArray[i] = int.Parse(Console.ReadLine());
+   var IsElement = int.TryParse(Console.ReadLine(), out ArrayElement);
+    
+    if (IsElement && ArrayElement > 0)
+    {
+        MyArray[i] = ArrayElement;
+    }
+    else {
+        return;
+    }
 }
 
+
 Console.WriteLine("Enter index of array");
-Index = int.Parse(Console.ReadLine());
+IsIndex = int.TryParse(Console.ReadLine(), out Index);
+if (!IsIndex || Index < 0) return;
 
 ArrayIndexSun = ArraySum(MyArray, Index);
-
 Console.WriteLine("Sum index of array is "+ ArrayIndexSun);
 
 
 int ArraySum(int[] Array, int Index)
 {
-    var sum = 0;
-
-    if (Array.Length < Index)
-        return -1;
-
-    
-    for (int i = 0; i < Index; i++)
+    if(Array.Length < Index)
     {
-        sum += Array[i];
+        return -1;
     }
+    var Sum = 0;
 
-    return sum;
+    string Element = Array[Index].ToString();
+
+    for (int i = 0; i < Element.Length; i++)
+    {
+        Sum += Convert.ToInt32(new string(Element[i], 1));
+    }
+    
+    return Sum;
 }
